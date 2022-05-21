c9d4e6c042e06ba9de4de7aee90638e718885ccd
LilJujun <103842011+LilJujun@users.noreply.github.com>
2022-05-21 16:00:17 +0400

    "check prikol KEKW))))"

--- rgr/src/main/java/com/example/rgr/RgrApplication.java
+++ rgr/src/main/java/com/example/rgr/RgrApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RgrApplication {
+//	HELLO EVERYBODY
 
 	public static void main(String[] args) {
 		SpringApplication.run(RgrApplication.class, args);
