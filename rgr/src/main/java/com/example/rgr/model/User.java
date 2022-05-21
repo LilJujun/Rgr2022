15098961ba222ddc8a5505f3840436827945e17f
Katik0o <104070213+Katik0o@users.noreply.github.com>
2022-05-21 17:43:14 +0400

    class user (conctructor, getters/setters)

--- rgr/src/main/java/com/example/rgr/model/User.java
+++ rgr/src/main/java/com/example/rgr/model/User.java
@@ -1,4 +1,80 @@
 package com.example.rgr.model;
 
+import javax.persistence.*;
+
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column( name = "ID", nullable = false)
+
+    private Long ID;
+    private String nickname;
+    private String email;
+    private String password;
+    private String photo;
+    private String status;
+
+    public User() {
+    }
+
+    public User(Long ID, String nickname, String email, String password, String photo, String status) {
+        this.ID = ID;
+        this.nickname = nickname;
+        this.email = email;
+        this.password = password;
+        this.photo = photo;
+        this.status = status;
+    }
+
+    public Long getID() {
+        return ID;
+    }
+
+    public void setID(long ID) {
+        this.ID = ID;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+
+
 }
