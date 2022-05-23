4f6ea265d4b9d92b30638b1dfa1e5362ee0b19b4
AoaGrigory <100870053+AoaGrigory@users.noreply.github.com>
2022-05-23 21:30:35 +0400

    Front

--- react-rgr/src/components/Messege/Message.jsx
+++ react-rgr/src/components/Messege/Message.jsx
@@ -6,35 +6,52 @@ import Header from '../Header/Header';
 import Navbar from '../Navbar/Navbar';
 
 const DialogItem = (props) => {
-  let path ="/ms/*";
+  let path = "/ms/*";
   return (
     <div className={s.item}>
       <NavLink to={path} >{props.name}</NavLink>
     </div>
   )
 }
-const Messsage = (props) =>{
-  return(
+const Messsage = (props) => {
+  return (
     <div className={s.messeng}>
-          {props.messsage}
-        </div>
+      {props.messsage}
+    </div>
   )
 }
 
 const Message = () => {
+  let dialogData = [
+    
+      {id: '1', name: 'Eldar'},
+      {id: '2', name: 'Grisha'},
+      {id: '3', name: 'Katya'}
+    
+  ]
+
+  const messageData=[
+    {id:'1',message:'Hello Wrold!!'},
+    {id:'2', message:'I wanna die'}
+  ]
+  
+  
+let dialogsElement = dialogData.map(dialog => <DialogItem name={dialog.name} id={dialog.id}/>);
+let messegesData = messageData.map(message => <Message name={message.name} />);
+
+
+
   return (
     <div className={s.messeng}>
 
       <div className={s.dialogs}>
         <div className={s.dialogsItem}>
 
-          <DialogItem name="Eldar" id='1' />
-          <DialogItem name="Katya" id='2' />
-          <DialogItem name="Grisha" id='3' />
-          
+          {dialogsElement}
+
 
         </div>
-        <Messsage messsage="Hello world" />
+        {messegesData}
 
       </div>
 
